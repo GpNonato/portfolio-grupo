6278268662d51f450e5ae9bd0a1c9887d16bb8cb
Gabriel Nonato <gabrielpnonato@gmail.com>
2026-03-05 20:07:52 -0300

    Jogo do pinto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import AppRoutes from "./routes/AppRoutes";
+import FlappyWidget from "./components/Flappy/FlappyWidget";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <FlappyWidget />
+    </>
+  );
 }
 
 export default App;
